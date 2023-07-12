17df6f773863c99d1606fc8472fbdc08082d554e
jonas <fsosa@nirsa.com>
2023-07-11 21:42:44 -0500

    first update

---
+++ Repository/ClienteRepository.cs
@@ -0,0 +1,180 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Repository
+{
+    public class ClienteRepository
+    {
+        public async Task<Cliente>  agregarCliente()
+        {
+            Console.WriteLine("Metodo agregar cliente");
+            SchoolContext context = new SchoolContext();
+            Cliente cliente = new Cliente();
+
+            Console.WriteLine("Nombre:");
+            cliente.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Apellido:");
+            cliente.Apellido = Console.ReadLine();
+
+            Console.WriteLine("Dirección:");
+            cliente.Direccion = Console.ReadLine();
+
+            Console.WriteLine("Teléfono:");
+            cliente.Telefono = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("Fecha de Nacimiento:");
+                cliente.FechaNacimiento = DateTime.Parse(Console.ReadLine());
+
+            }
+            catch
+            {
+
+            }
+
+            context.Clientes.Add(cliente);
+            context.SaveChanges();
+
+            Console.WriteLine("Codigo: " + cliente.Id + " Nombre: " + cliente.Nombre);
+
+            return cliente;
+
+        }
+
+        public async Task<List<Cliente>> consultarClientes()
+        {
+            Console.WriteLine("Metodo consultar estudiantes");
+            SchoolContext context = new SchoolContext();
+            List<Cliente> listClientes = context.Clientes.ToList();
+
+            foreach (var item in listClientes)
+            {
+                Console.WriteLine("Codigo: " + item.Id + " Nombre: " + item.Nombre);
+            }
+
+            return listClientes;
+
+        }
+
+        public async Task<Cliente> consultarCliente()
+        {
+            Console.WriteLine("Metodo consultar cliente por Id");
+            SchoolContext context = new SchoolContext();
+            Cliente cliente = new Cliente();
+
+            Console.WriteLine("Id ");
+
+            Int32 Id = Int32.Parse(Console.ReadLine());
+
+            cliente = context.Clientes.Find(Id);
+
+            Console.WriteLine("Codigo: " + cliente.Id + " Nombre: " + cliente.Nombre);
+
+            return cliente;
+
+        }
+
+        public async Task<Cliente> modificarCliente()
+        {
+            Console.WriteLine("Metodo modificar cliente");
+            SchoolContext context = new SchoolContext();
+            Cliente cliente = new Cliente();
+
+            Console.WriteLine("Ingrese Id ");
+            Int32 Id = Int32.Parse(Console.ReadLine());
+
+            cliente = context.Clientes.Find(Id);
+
+            if (cliente == null)
+            {
+                throw new Exception("Cliente no existe");
+            }
+
+            Console.WriteLine("Nombre:");
+            cliente.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Apellido:");
+            cliente.Apellido = Console.ReadLine();
+
+            Console.WriteLine("Dirección:");
+            cliente.Direccion = Console.ReadLine();
+
+            Console.WriteLine("Teléfono:");
+            cliente.Telefono = Console.ReadLine();
+
+
+            Console.WriteLine("Fecha de Nacimiento:");
+            cliente.FechaNacimiento = DateTime.Parse(Console.ReadLine());
+
+
+
+            context.SaveChanges();
+            Console.WriteLine("Codigo: " + cliente.Id + " Nombre: " + cliente.Nombre);
+            return cliente;
+
+        }
+
+        public async void eliminarCliente()
+        {
+            Console.WriteLine("Eliminación de clientes");
+            SchoolContext context = new SchoolContext();
+            Cliente std = new Cliente();
+
+            Console.WriteLine("Ingrese Id: ");
+
+            Int32 Id = Int32.Parse(Console.ReadLine());
+
+            std = context.Clientes.Find(Id);
+            context.Remove(std);
+            context.SaveChanges();
+            Console.WriteLine("Codigo: " + std.Id + " Nombre: " + std.Nombre);
+
+     
+
+        }
+        public async void consultarClientesFunciones()
+        {
+            Console.WriteLine("Metodo consultar clientes con el uso de funciones");
+            SchoolContext context = new SchoolContext();
+            List<Cliente> listClientes;
+
+            Console.WriteLine("Cantidad de registros: " + context.Clientes.Count());
+            Cliente cliente = context.Clientes.First();
+
+            Console.WriteLine("Primer elemento de la tabla:" + cliente.Id + "-" + cliente.Nombre);
+
+            //listEstudiantes = context.Students.Where(s => s.StudentId > 2 && s.Name == "Anita").ToList();
+
+            //listEstudiantes = context.Students.Where(s => s.Name == "Patty" || s.Name == "Anita").ToList();
+
+            listClientes = context.Clientes.Where(s => s.Nombre.StartsWith("A")).ToList();
+
+            /*
+            var query = context.Students.GroupBy( s => s.Name) 
+            .Select(g => new
+            {
+                Nombre = g.Key,
+                Cantidad = g.Count()
+            }). ToList();
+
+            foreach (var result in query)
+            {
+                Console.WriteLine($"Nombre: {result.Nombre}, Cantidad: {result.Cantidad}");
+            }
+            */
+
+
+            foreach (var item in listClientes)
+            {
+                Console.WriteLine("Codigo: " + item.Id + " Nombre: " + item.Nombre);
+            }
+
+        }
+    }
+}
